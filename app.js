831765c4e93b98e172aad85f138120cb762ee0bd
gregoryqp <66794801+gregoryqp@users.noreply.github.com>
2024-01-24 08:03:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -170,7 +170,7 @@ function generatePDF(){
   const {jsPDF} = window.jspdf;
   let doc = new jsPDF();
   let logo = new Image();
-   logo.src = "/logo.PNG";
+   logo.src = "logo.PNG";
   doc.addImage(logo, 'PNG', 10, 10, 100, 20);
   doc.setFontSize(12);
   
